ed38ac51b900d7de7d4b4c86d59720fb239abfb4
Pepe <josepintomuo@gmail.com>
2023-09-08 18:33:41 -0300

    menu

--- LAB 1/node.py
+++ LAB 1/node.py
@@ -1,21 +1,18 @@
 import socket
 import sys
+import random
 
 # Dirección IP y puerto del cliente
 client_server_ip = "localhost" #vacío para localhost
-client_server_port = 5000
+client_server_port = 5001
 
 # Dirección IP y puerto del servidor host
 host_server_ip = "" #vacío para localhost
-host_server_port = 5000
+host_server_port = 5001
 
 def connect_to_client(client_address):
     node_to_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
     try:
-        """
-        El método connect intenta establecer una conexión con el cliente.
-        """
         node_to_client_socket.connect(client_address)
     except socket.error as message:
         print("Error al conectar al cliente en la dirección:", client_address)
@@ -27,9 +24,9 @@ def connect_to_client(client_address):
     return node_to_client_socket
 
 def send_message(socket, message):
-    # Enviar mensaje
     socket.send(message.encode())
-    pass
+    return
+
 
 def receive_message(socket):
     message = socket.recv(1024)
@@ -49,23 +46,44 @@ def main():
     
     while True:
         client_socket, client_address = intermediary_server_socket.accept()
-        print(f"Conexión establecida con el cliente en {client_address}")
-        
+        print("Conexión establecida con el cliente:", client_address)
+
+        # Conexion con el conecta4 server
+        '''
         connecta4_server_address = ('connecta4_server_ip', client_server_port)
         connecta4_server_socket = connect_to_connecta4_server(connecta4_server_address)
+        '''
+        randint = random.randint(0, 1)
+        if randint == 0:
+            menssage = "NO"
+
+        else:
+            menssage = "OK"
+
+        send_message(client_socket, menssage)
+        print("respuesta de disponibilidad:", menssage)
         
-        try:
-            while True:
-                # Implementa la lógica para comunicarte con el Cliente y el Servidor Conecta4
-                pass
-        
-        except Exception as e:
-            print("Error en la comunicación:", e)
-        
-        finally:
-            print("Cerrando la conexión con el cliente y el Servidor Conecta4...")
+        #Cerrar la conexión con el cliente y cerrar el servidor
+        if menssage == "NO":
             client_socket.close()
-            connecta4_server_socket.close()
+            print("Conexión con el cliente cerrada.")
+            break
+        
+        elif menssage == "OK":
+            print("Jugando...")
+            print("Jugando...")
+            print("Jugando...")
+
+
+        client_socket.close()
+        print("Conexión con el cliente cerrada.")
+        break
+
+
+    intermediary_server_socket.close()
+    print("Servidor cerrado.")
+    return
+
 
 if __name__ == "__main__":
     main()
--- LAB 1/player.py
+++ LAB 1/player.py
@@ -3,23 +3,23 @@ import sys
 
 # Dirección IP y puerto del servidor intermediario
 intermediary_server_ip = "192.168.1.12"
-intermediary_server_port = 5000
+intermediary_server_port = 5001
 
+
+# Conecta con el servidor intermediario
 def connect_to_server(server_address):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         # Conectar el socket del cliente con el servidor en el puerto indicado
         client_socket.connect(server_address)
-        print("Mati chupame el pico")
     except socket.error as message:
         print("Falló la conexión con el servidor {} por el puerto {}".format(server_address[0], server_address[1]))
         print(message)
         sys.exit()
     return client_socket
 
+
 def send_message(socket, message):
-    # Enviar mensaje
     socket.send(message.encode())
     return
 
@@ -30,7 +30,61 @@ def receive_message(socket):
 
 def disconnect_to_server(socket):
     socket.close()
+    return
 
+
+def menu():
+    try:
+        while True:
+            print("- - - - - - - - Bienvenido al Juego - - - - - - - -")
+            print("- Seleccione una opción:")
+            print("1-Jugar")
+            print("2-Salir")
+            
+            choice = input(">>")
+            if choice == "1":
+                # Iniciamos conexión con el servidor intermediario
+                server_address = (intermediary_server_ip, intermediary_server_port)
+                client_socket = connect_to_server(server_address)
+
+                # Enviamos mensaje de disponibilidad
+                send_message(client_socket, "disponible")
+
+                # Recibimos respuesta de disponibilidad
+                response = receive_message(client_socket)
+
+                if response == "NO":
+                    print("respuesta de disponibilidad:", response)
+                    print("El servidor no está disponible. Intente más tarde.")
+                    disconnect_to_server(client_socket)
+                    break
+                
+
+                elif response == "OK":
+                    print("respuesta de disponibilidad:", response)
+                    print("- - - - - - - - Comienza el Juego - - - - - - - -")
+                    print("Jugando...")
+                    print("Jugando...")
+                    print("Jugando...")
+            
+            
+                    # Cerramos la conexión con el servidor
+                    disconnect_to_server(client_socket)
+                    print("Saliendo del juego.")
+                    break
+
+            elif choice == "2":
+                print("Saliendo del juego.")
+                break
+                
+            else:
+                print("Opción inválida. Por favor, elija nuevamente.")
+
+    except Exception as e:
+        print("Error al conectar con el servidor:", e)
+        sys.exit()
+    return
+    
 
 def print_board(board):
     cols = len(board[0])
@@ -44,51 +98,8 @@ def print_board(board):
     return
 
 def main():
-    server_address = (intermediary_server_ip, intermediary_server_port)
-    client_socket = connect_to_server(server_address)
-    print("Mati chupame el pico")
-    
-    try:
-        print("Mati chupame el pico")
-        while True:
-            print("- - - - - - - - Bienvenido al Juego - - - - - - - -")
-            print("- Seleccione una opción:")
-            print("1-Jugar")
-            print("2-Salir")
-            
-            choice = input(">>")
-            
-            if choice == "1":
-                connect_to_server(server_address)
-                board=create_board(6,6)
-                '''
-                print("- - - - - - - - Comienza el Juego - - - - - - - -")
-                col = int(input(">>"))
-                #print("Seleccione un y:")
-                #y = int(input(">>"))
-                played = False
-                while not played:
-                    print_board(board)
-                    print("Seleccione columna:")
-                    board,played=modify_board(board, col, "X")
-                print_board(board)
-                if (check_winner(board) == True):
-                    print("Ganaste")
-                    '''
-                pass
-                
-            elif choice == "2":
-                print("Saliendo del juego.")
-                break
-                
-            else:
-                print("Opción inválida. Por favor, elija nuevamente.")
-
-    except Exception as e:
-        print("Error al conectar con el servidor:", e)
-    
-    finally:
-        client_socket.close()
+    menu()
+    return
 
 if __name__ == "__main__":
     main()
